3bbd0202c9521b935861a1ef39c738fa4eeac216
et18 <etureci@cafe.CS.Princeton.EDU>
2019-05-02 14:41:52 -0400

    removed unnecessary DEC_Pipeline tag in vertex_nominations kernel; adding some data

--- vertex_nomination/vertex_nomination_DECADES.py
+++ vertex_nomination/vertex_nomination_DECADES.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-"""
-
-main.py
-
-"""
 
 from __future__ import division, print_function
 import argparse
@@ -17,9 +12,9 @@ from scipy.sparse import csr_matrix
 from DEC_Pipeline import DEC_Pipeline
 from DEC_Numba_Lib import DecSparseGraph, LoadDecSparseGraph, DecSparseGraphSpec
 
-@njit(int32[:](DecSparseGraphSpec(), int32[:], int32), nogil=True)
-def multi_bfs(graph, roots, num_nodes):
-    node_visit_list = np.ones(num_nodes, dtype=np.int32) * np.int32(-1)
+@njit(int32[:](DecSparseGraphSpec(), int32[:]), nogil=True)
+def multi_bfs(graph, roots):
+    node_visit_list = np.ones(graph.num_nodes, dtype=np.int32) * np.int32(-1)
     top_layer = list(roots)
     next_layer = [np.int32(x) for x in range(0)]
     hop = 1
@@ -38,11 +33,11 @@ def multi_bfs(graph, roots, num_nodes):
     return node_visit_list
 
 
-@njit(float64[:](DecSparseGraphSpec(), int32[:], int32), nogil=True)
-def getContentScore(graph, roots, num_nodes):
+@njit(float64[:](DecSparseGraphSpec(), int32[:]), nogil=True)
+def getContentScore(graph, roots):
     top_layer = list(roots)
     # get # of direct connection score
-    directConnectionScore =  np.zeros(num_nodes, dtype=np.int32)
+    directConnectionScore =  np.zeros(graph.num_nodes, dtype=np.int32)
     for each_node in top_layer:
         for e in graph.indices[graph.indptr[each_node]:graph.indptr[each_node + 1]]:
             directConnectionScore[e] +=1
@@ -58,15 +53,15 @@ def getContentScore(graph, roots, num_nodes):
     return contentscore
 
 
-@njit(Tuple((int32, float32))(DecSparseGraphSpec(), int32[:], int32), nogil=True, pipeline_class=DEC_Pipeline)
-def vertex_nomination_kernel_(G, seeds, num_nodes):
+@njit(Tuple((int32, float32))(DecSparseGraphSpec(), int32[:]), nogil=True)
+def vertex_nomination__kernel__(G, seeds):
     
     # context score : computed via distance map from multi BFS
-    bfs_results = multi_bfs(G, seeds,num_nodes)
+    bfs_results = multi_bfs(G, seeds)
     context_sim = 1/bfs_results
 
     # content score : placeholder computed via a random number generator
-    content_sim = getContentScore(G, seeds, num_nodes)
+    content_sim = getContentScore(G, seeds)
 
     # fusion score : content_sim * context_sim 
     fusion_score = np.multiply(content_sim, context_sim)
@@ -98,7 +93,7 @@ if __name__ == "__main__":
     t = time()
 
     # load graph
-    G, num_nodes = LoadDecSparseGraph(args.inpath)
+    G = LoadDecSparseGraph(args.inpath)
     print('load time = %f' % (time() - t))
 
     # initialize set of seeds
@@ -107,26 +102,26 @@ if __name__ == "__main__":
 
     # get number of nodes in graph
     num_nodes_connected = len(G.indptr)
-    print('num_nodes:',num_nodes_connected)
-
+    print('number of nodes:',num_nodes_connected)
+    print('number of nodes from G:', G.num_nodes)
     # get seeds
     seed_input = input('Please enter five seeds separated by comma:')
     if seed_input:
         seeds = list(map(int,seed_input.split(',')))
-        if len(seeds)<5 or all(i >= num_nodes for i in seeds):
+        if len(seeds)<5 or all(i >= G.num_nodes for i in seeds):
             print('Incorrect input entry!')
             raise
     else:
         # get random nodes in graph:
         seeds = np.random.choice(num_nodes_connected, 5, replace=False)
 
-    seeds=np.array(list(seeds), dtype=np.int32)
-    print('Starting with seeds:',seeds)
+    seeds = np.array(list(seeds), dtype=np.int32)
+    print('Starting with seeds:', seeds)
 
 
     # Vertex Nomination
     start = time()
-    top_nominee, top_score = vertex_nomination_kernel_(G, seeds, num_nodes)        
+    top_nominee, top_score = vertex_nomination__kernel__(G, seeds)        
     end = time()
     print('Top nominee:',top_nominee,';Top score:',top_score)
     print("Compute time: " + str(end - start))
